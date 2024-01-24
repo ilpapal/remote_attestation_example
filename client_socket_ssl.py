9b54ced07dc1c86e90efb0f249e222b28aca2b43
Ilias Papalamprou <iliaspapalambrou@gmail.com>
2024-01-24 12:07:34 +0200

    Minor Changes

--- client_socket_ssl.py
+++ client_socket_ssl.py
@@ -162,16 +162,22 @@ def main():
             client_ecdh = DiffieHellman()
 
             # Exchange public keys with the client
-            public_key_received = secure_client_socket.recv(128)
+            public_key_received = secure_client_socket.recv(1024)
             public_key_received_utf8 = public_key_received.decode('utf-8')
             public_key_hex = get_key_hex(client_ecdh.public_key)
             secure_client_socket.sendall(public_key_hex.encode('utf-8'))
 
+            print("==========")
+            print(public_key_hex)
+            print("==========")
+            print(public_key_received_utf8)
+            print("==========")
 
-            # data_received = secure_client_socket.recv(1024)
-            # data_received_utf8 = data_received.decode('utf-8')
-            # data_received_bytes = bytes.fromhex(data_received_utf8)
-            # bitstr_decryption_key = client_ecdh.decrypt(server_ecdh_pub_key, data_received_bytes, client_ecdh.IV)
+            # Complete the key exchange
+            data_received = secure_client_socket.recv(1024)
+            data_received_utf8 = data_received.decode('utf-8')
+            data_received_bytes = bytes.fromhex(data_received_utf8)
+            bitstr_decryption_key = client_ecdh.decrypt(server_ecdh_pub_key, data_received_bytes, client_ecdh.IV)
 
             # Decrypt the bitstream and build the xclbin file
             print("#################################################################")
